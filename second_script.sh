606e25e168772cd3d20042c37f8957fee1979779
drangons <dikshithsh@hotmail.com>
2013-07-12 14:34:22 +0200

    check the real and effective user id

---
+++ second_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+set -X
+# this is the second example shell script which is being committed to git.
+#Jenkins should be able to pickup any changes to this repo every 5 mins and 
+# should start a build.
+
+#put some example codes here.
+
+echo "My real user id is $UID"
+echo "My effective user id is $EUID"
+
+exit 0
